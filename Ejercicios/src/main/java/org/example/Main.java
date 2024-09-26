0995b43badab978ab85509004f71c439c888fe92
A703\1dam <danielleludovicoo@gmail.com>
2024-09-26 16:33:13 +0200

    ejercicios teoricos

--- Ejercicios/src/main/java/org/example/Main.java
+++ Ejercicios/src/main/java/org/example/Main.java
@@ -8,7 +8,15 @@ public class Main {
 
         //Activ_inicial activ_inicial = new Activ_inicial();
         //activ_inicial.ejecutar();
-         Ejercicio1 ejercicio1 = new Ejercicio1();
-        ejercicio1.ejecutar();
+        // Ejercicio1 ejercicio1 = new Ejercicio1();
+        //ejercicio1.ejecutar();
+
+        Operadores operadores = new Operadores();
+       // operadores.operadores();
+       // operadores.asignacion();
+       // operadores.comparacion();
+
+        operadores.logicos();
+
     }
 }
