fa89831098eb625b6aae0813e82b7233b094f883
A703\1dam <danielleludovicoo@gmail.com>
2024-10-24 17:10:14 +0200

    subo ejercicios de repaso de examen

---
+++ Ejercicios/src/main/java/org/example/Baterias.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Baterias {
+
+    public void ejercicio1(){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Dime un número: ");
+
+        int numero = teclado.nextInt();
+
+        if (numero )
+
+
+
+    }
+}
